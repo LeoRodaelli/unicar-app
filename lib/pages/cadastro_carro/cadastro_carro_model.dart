1c918fd2e84e3ee4598d8d85e4b005e3b0b28512
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-03 16:39:40 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/cadastro_carro/cadastro_carro_model.dart
+++ lib/pages/cadastro_carro/cadastro_carro_model.dart
@@ -1,4 +1,3 @@
-import '/auth/custom_auth/auth_util.dart';
 import '/backend/api_requests/api_calls.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
@@ -7,6 +6,7 @@ import '/flutter_flow/flutter_flow_widgets.dart';
 import 'cadastro_carro_widget.dart' show CadastroCarroWidget;
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -31,8 +31,8 @@ class CadastroCarroModel extends FlutterFlowModel<CadastroCarroWidget> {
   FocusNode? cNHdocondutorFocusNode;
   TextEditingController? cNHdocondutorController;
   String? Function(BuildContext, String?)? cNHdocondutorControllerValidator;
-  // Stores action output result for [Backend Call - API (Cadastrar carro)] action in Button widget.
-  ApiCallResponse? registerCarApiResponse;
+  // Stores action output result for [Backend Call - API (Cadastro de usuario)] action in Button widget.
+  ApiCallResponse? apiResultrjr;
 
   /// Initialization and disposal methods.
 
