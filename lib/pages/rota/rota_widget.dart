3fae32925cad93864ea5c77164d5ba377fddc721
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-04 03:13:24 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/rota/rota_widget.dart
+++ lib/pages/rota/rota_widget.dart
@@ -67,29 +67,6 @@ class _RotaWidgetState extends State<RotaWidget> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              FFButtonWidget(
-                onPressed: () async {
-                  context.pushNamed('CadastroCarro');
-                },
-                text: 'Button',
-                options: FFButtonOptions(
-                  height: 40.0,
-                  padding: EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
-                  iconPadding:
-                      EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                  color: FlutterFlowTheme.of(context).primary,
-                  textStyle: FlutterFlowTheme.of(context).titleSmall.override(
-                        fontFamily: 'Readex Pro',
-                        color: Colors.white,
-                      ),
-                  elevation: 3.0,
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                    width: 1.0,
-                  ),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),
               Padding(
                 padding: EdgeInsetsDirectional.fromSTEB(20.0, 0.0, 20.0, 10.0),
                 child: FlutterFlowPlacePicker(
