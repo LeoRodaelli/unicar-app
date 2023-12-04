7c11532ea4a1529ab424f32dae6c4767288044d9
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-04 03:19:55 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/caronas_disponiveis/caronas_disponiveis_widget.dart
+++ lib/pages/caronas_disponiveis/caronas_disponiveis_widget.dart
@@ -50,6 +50,8 @@ class _CaronasDisponiveisWidgetState extends State<CaronasDisponiveisWidget> {
       );
     }
 
+    context.watch<FFAppState>();
+
     return GestureDetector(
       onTap: () => _model.unfocusNode.canRequestFocus
           ? FocusScope.of(context).requestFocus(_model.unfocusNode)
